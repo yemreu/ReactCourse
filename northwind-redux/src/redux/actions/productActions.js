333b714d44a31e055eb398d8ede958ce293736da
yemreu <yemreu@users.noreply.github.com>
2021-03-21 18:24:48 +0300

    Post Actions, Handler

--- northwind-redux/src/redux/actions/productActions.js
+++ northwind-redux/src/redux/actions/productActions.js
@@ -1,16 +1,63 @@
 import * as actionTypes from "./actionTypes";
 
-export function getProductsSuccess(products){
-    return {
-        type: actionTypes.GET_PRODUCTS_SUCCESS,
-        payload: products
-    } 
+export function getProductsSuccess(products) {
+  return {
+    type: actionTypes.GET_PRODUCTS_SUCCESS,
+    payload: products,
+  };
+}
+
+export function createProductSuccess(product) {
+  return {
+    type: actionTypes.CREATE_PRODUCT_SUCCESS,
+    payload: product,
+  };
+}
+
+export function updateProductSuccess(product) {
+  return {
+    type: actionTypes.UPDATE_PRODUCT_SUCCESS,
+    payload: product,
+  };
+}
+
+export function saveProductApi(product) {
+  return fetch("http://localhost:3000/products/" + (product.id || ""), {
+    method: product.id ? "PUT" : "POST",
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify(product),
+  })
+    .then(handleResponse)
+    .catch(handleError);
+}
+
+export function saveProduct(product){
+  return function(dispatch){
+    saveProductApi(product).then(savedProduct=>{
+      product.id?dispatch(updateProductSuccess(saveProduct)):dispatch(createProductSuccess(savedProduct))
+      .catch(error=>{throw error})
+    });
+  }
+}
+
+export async function handleResponse(response){
+  if(response.ok){
+    return response.json();
+  }
+
+  const error = await response.text();
+  throw new Error(error);
+}
+
+export function handleError(error){
+  console.error("Bir hata oluştu");
+  throw error;
 }
 
 export function getProducts(categoryId) {
   return function (dispatch) {
     let url = "http://localhost:3000/products";
-    if(categoryId){
+    if (categoryId) {
       url += "?categoryId=" + categoryId;
     }
     return fetch(url)
